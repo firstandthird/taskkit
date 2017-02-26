e9619fb108502f0761f6192452f7e8b99a524fb8
Greg Allen <greg@firstandthird.com>
2017-02-26 14:19:48 -0800

    add linting

--- bin.js
+++ bin.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+'use strict';
 const main = require('./index.js');
 main();
--- test/test.core.js
+++ test/test.core.js
@@ -4,4 +4,5 @@ const test = require('tape');
 test('instance of', (t) => {
   // t.plan(1);
   // t.equal(nt instanceof ClientKitTask, true, 'instance of ClientKitTask');
+  t.end();
 });
