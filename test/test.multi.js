6cf8934e2423288ba952bbc5733c6968311cca84
orthagonal <orthagonal1@gmail.com>
2018-01-06 21:40:36 -0600

    do not need wait in test

--- test/test.multi.js
+++ test/test.multi.js
@@ -24,7 +24,6 @@ tap.test(' lets you run a list of multiple tasks', async(t) => {
   process.env.TASKKIT_PREFIX = 'default';
   process.env.TASKKIT_CONFIG = path.join(__dirname, 'conf_multi');
   await main();
-  await wait(1500);
   t.notEqual(results.indexOf('             ls  ::  Running ls...'), -1);
   t.notEqual(results.indexOf('           free  ::  Running free...'), -1);
   t.end();
